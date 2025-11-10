576add93117fdc15dff89fcd3a4639a70050146b
susheeldehariya224-cpu <susheeldehariya224@gmail.com>
2025-11-10 13:10:07 +0530

    .......

--- hello.java
+++ hello.java
@@ -1,10 +1,66 @@
 class main{
-public static void main(String[] args)
-{
-    StringBuilder str=new StringBuilder(str:"hello");
+    // public static void main(string[] args){
+    //     string str="hello java";
+    //   system.out.println(str.charAt(index:1));
+    //     system.out.println(str.toUpperCase());
+    //     system.out.println(str.toUpperCase());
+    //     system.out.println(str.indexOf(str:"z"));  system.out.println(str.length());
+    //     
+    //     system.out.println(str.substring(beginindex :2));
+    //     system.out.println(str.substring(beginindex:1,endIndex:2));
+    //     system.out.println(str.length());
+    //     system.out.println(str.traim());
 
-    str.insert(offset:2,str:"byee");
-    System.out.println(str);
-    
-}
+    // }
+
+//  system.out.println(str.charAt(index:1));
+    //     system.out.println(str.toUpperCase());
+    //     system.out.println(str.toUpperCase());
+    //     system.out.println(str.indexOf(str:"z"));  system.out.println(str.length());
+    //     
+    //     system.out.println(str.substring(beginindex :2));
+    //     system.out.println(str.substring(beginindex:1,endIndex:2));
+    //     system.out.println(str.length());
+    //     system.out.println(str.traim());
+
+
+
+  // String a="abc";
+  // String b="";
+  // for(int i=a.length()-1;i>=0;i--){
+  //   b+=a.charAt(i);
+
+  // }
+  // if (a.equals (b)){
+  //   System.out.println(x:"ji haii");
+
+  // }else{
+  //   System.out.println(x:"nhi hai");
+  // }
+
+
+
+//   String a="abcgyeuio";
+//   String b="";
+//   for(int i=a.length()-1;i>=0;i--){
+//     b+=a.charAt(i);
+
+//   }
+//   if (a.equals (b)){
+//     System.out.println(x:"ji haii");
+
+//   }else{
+//     System.out.println(x:"nhi hai");
+//   }
+
+ public static void main(string[] args){
+  String str="hello java cpp";
+  Stringa[]=str.split(regex:" ")
+  System,out,println (a[1]);
+  for (string str:1)
+  System.out.println(str1); 
+
+
+
+
 }
