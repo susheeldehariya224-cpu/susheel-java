d404e82d0cc5234fd8a924c19c34092adca79785
susheeldehariya224-cpu <susheeldehariya224@gmail.com>
2025-11-13 21:37:16 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,198 @@
+
+// class Main {
+//     public static void main(String[] args) {
+//         // 5
+//         // 55
+//         // 555
+//         // 5555
+//         // Scanner sc =new Scanner(System.in);
+//         // System.out.println("enter value of a");
+//         // int a= sc.nextInt();
+//         // System.out.println("enter value of b");
+// // class Main {
+// //     public static void main(String[] args) {
+// //         StringBuilder s1=new StringBuilder("hello");
+// //         // s1.insert(1, "bye");
+// //         s1.
+// //         // System.out.println(s1);
+// //         // System.out.println(s1.append(false));
+// //         System.out.println(s1);
+
+//         // int b= sc.nextInt();
+// //         // System.out.println(s1.in);
+
+//         // System.out.println(a);
+// //         // 5
+// //         // 55
+// //         // 555
+//         // 5555
+//         // Scanner sc =new Scanner(System.in);
+//         // System.out.println("enter value of a");
+//         // int a= sc.nextInt();
+//         // System.out.println("enter value of b");
+
+//         // int b= sc.nextInt();
+
+        // int a=123;
+        //  byte a=5;
+        //  byte b=6;
+        //  byte c=(byte)a+b;
+//         // System.out.println(a);
+
+        // int   a=5;
+        // short b=(short)a;
+        // System.out.println(b);
+        // short a=5;
+        // byte b=(byte)a;
+
+//         // int a=123;
+//         //  byte a=5;
+//         //  byte b=6;
+//         //  byte c=(byte)a+b;
+
+//         // int   a=5;
+//         // short b=(short)a;
+//         // System.out.println(b);
+//         // short a=5;
+//         // byte b=(byte)a;
+
+
+
+        // for(int i=0;i<5;i++){
+        // System.out.println(i);
+        // }
+
+
+//         // for(int i=0;i<5;i++){
+//         // System.out.println(i);
+//         // }
+
+
+
+
+        // int a=5;
+        // if(a%2==0){
+        //     System.out.println("");
+        // }else{
+        //     System.out.println("oddd");
+        // }
+//         // int a=5;
+//         // if(a%2==0){
+//         //     System.out.println("");
+//         // }else{
+//         //     System.out.println("oddd");
+//         // }
+
+
+
+
+
+        // byte  a=130;
+        // short b=5;
+        // int c=5;
+        // float f=90.3f;
+        // boolean data=false;
+        // System.out.println(b);
+        // java => JDK=> JVM, JRE
+        //  hello.java=>
+        // int a=5;
+        // int b=10;
+//         // byte  a=130;
+//         // short b=5;
+//         // int c=5;
+//         // float f=90.3f;
+//         // boolean data=false;
+//         // System.out.println(b);
+//         // java => JDK=> JVM, JRE
+//         //  hello.java=>
+//         // int a=5;
+//         // int b=10;
+
+        // 5
+        // 55
+        // 555
+        // 5555
+//         // 5
+//         // 55
+//         // 555
+//         // 5555
+
+        // if(a<4 &&   b==20){
+        //     System.out.println("even");
+//         // if(a<4 &&   b==20){
+//         //     System.out.println("even");
+
+        // }
+        // int a=5;
+        // int b=10;
+        // System.out.println(a+b);
+        // int a=5;
+        // if(a%2==0){
+        //      System.out.println("even");
+        // }else{
+        //      System.out.println("oddd");
+        // }
+
+    //    for(int i=0 ;i< 5; i++ ){
+    //     System.out.print(i);
+//         // }
+//         // int a=5;
+//         // int b=10;
+//         // System.out.println(a+b);
+//         // int a=5;
+//         // if(a%2==0){
+//         //      System.out.println("even");
+//         // }else{
+//         //      System.out.println("oddd");
+//         // }
+
+//     //    for(int i=0 ;i< 5; i++ ){
+//     //     System.out.print(i);
+
+    //    }
+    // byte a=10;
+    // byte b=20;
+    // byte c=a+b;
+    // System.out.println(c);
+//     //    }
+//     // byte a=10;
+//     // byte b=20;
+//     // byte c=a+b;
+//     // System.out.println(c);
+
+
+
+    // for(int i =0;i<5;i++){
+    //     for(int j=0;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println(" ");
+//     // for(int i =0;i<5;i++){
+//     //     for(int j=0;j<=i;j++){
+//     //         System.out.print("*");
+//     //     }
+//     //     System.out.println(" "
+
+
+
+public class main{
+        public static void main(String[] args) {
+                Manager m=new Manager();
+                m.work();
+                m.meeting();
+        }
+}
+public Employee {
+
+        void work()
+        {
+                System.out.println("Employee is working")
+                }
+}
+class manager extends Employee
+{
+        void meeting()
+        {
+                System.out.println("manager is meeting");
+        }
+}
