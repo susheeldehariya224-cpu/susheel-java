6820de752ba18e34d6b5d4798f73e9d62478fd42
susheeldehariya224-cpu <susheeldehariya224@gmail.com>
2025-11-20 12:47:39 +0530

    data.

--- hello.java
+++ hello.java
@@ -1,101 +1,47 @@
-// public class hello {
-//     static  int a=5;
+// package lec5;
 
-//     // public  void sum(){
-//     //     System.out.println("hello");
-//     // }
-//     public static void main(String[] args) {
-      
-//         // sum();
-//         hello obj =new hello();
-//         System.out.println(obj.a);
-//         // obj.sum();
-        
-//     }
+// public class hello {
     
 // }
 
 
 // class Main{
-// //    static  int a=5; 
-//  void  Sum(int a){
-//  // "aabbcd"
-//     System.out.println("hello");
-// }
-// static  int val(){
-//     return  0;
-
-// };
 //     public static void main(String[] args) {
-//         // Sum();
-//         Main obj=new Main();
-//         int data=9;
-//         obj.Sum(data);
-//         val();
-
-//         // System.out.println(a);
-//         // int a=5;
-//         // System.out.println(a);
-//         // System.out.println(a);
-//     }
-// }
-
-
-// public class hello{
+//         String str="aabbcc"; // a2b2c1
+//         StringBuilder result=new StringBuilder();
+//         int count=1;
+//         for(int i=1;i<str.length();i++){
+//             if(str.charAt(i)==str.charAt(i-1)){
+//                 count++;
+//             }else{
+//                 result.append(str.charAt(i-1)).append(count);
+//                 count=1;
+//             }
+//         }
+//         result.append(str.charAt(str.length()-1)).append(count);
+//         System.out.println(result);
+// class Main{
 //     public static void main(String[] args) {
-//         // Main obj=new Main();
-//         // System.out.println(obj);
-//         Car obj=new Car();
-//         obj.display();  
-//     }
-// }          
-//    class Car{
-//        String name="hiii";
-//      void display(){
-//         System.out.println(name);
-        
-//     }
-// }
+//         String str="aabbcc"; // a2b2c1
+//         StringBuilder result=new StringBuilder();
+//         int count=1;
+//         for(int i=1;i<str.length();i++){
+//             if(str.charAt(i)==str.charAt(i-1)){
+//                 count++;
+//             }else{
+//                 result.append(str.charAt(i-1)).append(count);
+//                 count=1;
+//             }
+//         }
+//         result.append(str.charAt(str.length()-1)).append(count);
+//         System.out.println(result);
 
-// class hello{
-// //   static  int a=5;
-// //    static  int data=20;
-//    static  int  Sum(int a){
-//         System.out.println("hello");
-//         return 0;
+
+
+// @@ -224,6 +219,6 @@ public static void main(String[] args) {
+//         }
+
+//         System.out.println(result);
 //     }
-
-//     public static void main(String[] args) {
-//         // Sum();
-//         // int a=5;
-//         Sum(5);
-//         // hello obj=new hello();
-//         // int data=5;
-//         // obj.Sum(data);
-//         // // System.out.println(a);
-//         // System.out.println(obj.data);
-
 //     }
 
-
-// }
-
-public class hello{
-class Car{
-     String name="hello";
-    static  void Sum(){
-        Car a=new Car();
-        System.out.println(a.name);
-    }
-}
-
-class Main{
-    public static void main(String[] args) {
-        Car obj =new Car();
-        obj.Sum();
-        
-    }
-
-}
-
-}
