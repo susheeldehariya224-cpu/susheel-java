16ac57a85a98d271f047a3354121c8c6cf1e2d96
susheeldehariya224-cpu <susheeldehariya224@gmail.com>
2025-12-12 14:10:18 +0530

    ............

---
+++ setters.java
@@ -0,0 +1,42 @@
+// package p1;
+// // collection
+
+
+// import java.util.ArrayList;
+
+//   class main{
+// public static void main(String[] args) {
+//     ArrayList<Integer> list=new ArrayList<>();
+//     // ArrayList<integer> list1=new ArrayList<>();
+// // list1.add(50);
+// list.add(5);
+// list.add(10);
+// list.add(1,30);
+// System.out.println(list);
+// // list.AddAll(list1);
+// // System.out.println(list);
+// }
+//   }
+
+
+
+
+
+// 5 .Remove operations
+
+
+import java.lang.reflect.Array;
+import java .util.ArrayList;
+public class setters
+{
+    public static void main(String[] args) {
+
+         ArrayList<Integer>list=new ArrayList<>();
+       list.add(300);
+       int first=list.get(0);
+
+int last=last.get(list.size()-1)
+list.set(32, 90);
+    }
+
+}
